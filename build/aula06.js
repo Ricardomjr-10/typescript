0a384cc552434a694258f7a6df0f7e82f5309524
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-04 21:23:29 -0300

    array somente leitura

--- build/aula06.js
+++ build/aula06.js
@@ -1,4 +1,12 @@
 "use strict";
-//readonly arrays
-//let numeros:number[] = [10, 20, 30]
+//readonly arrays - array somente leitura
 let numeros = [10, 20, 30];
+//let numeros: Array<number|string> = [10, 20, 30]
+//let numeros: (number | string)[] = [10, 20, 30, '40']
+numeros.push(40);
+numeros.unshift(50);
+numeros.pop();
+numeros.shift();
+console.log(numeros);
+let numeros_ro = [100, 200, 300];
+console.log(numeros_ro);
