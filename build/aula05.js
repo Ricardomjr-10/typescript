ee88bf3ee2de289053aca62cd0292d2a7c35a433
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-04 21:04:52 -0300

    types

--- build/aula05.js
+++ build/aula05.js
@@ -1,1 +1,14 @@
 "use strict";
+//union types - tipos diferentes na mesma variavel
+//let vteste: string | number | boolean
+let vteste;
+vteste = 'luiz';
+vteste = 10;
+vteste = true;
+let cursos = ['Javascript', 'typescript', 'c++', 100, false];
+let valores = [10, 20, 30];
+cursos.push('python');
+valores.push(40);
+console.log(cursos);
+console.log(valores);
+console.log(vteste);
