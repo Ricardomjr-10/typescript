156a7a0168b58ef5e050c68bcec28088f03de2bc
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-05 09:59:08 -0300

    exemplos

--- build/aula07.js
+++ build/aula07.js
@@ -1,1 +1,7 @@
 "use strict";
+//TUPLAS - tipos especificos para posicoes especificas no array
+let coisas = ['Luiz', 10, true];
+// coisas.push('Maria', 20, true)
+// coisas[2] = false
+console.log(coisas);
+console.log(coisas[2]);
---
+++ src/aula07.ts
@@ -0,0 +1,8 @@
+//TUPLAS - tipos especificos para posicoes especificas no array
+
+let coisas:readonly[string, number,boolean] = ['Luiz', 10, true]
+// coisas.push('Maria', 20, true)
+// coisas[2] = false
+
+console.log(coisas)
+console.log(coisas[2])
