e41bd1bd83d7bd640ca54dc89cc6d930002c6107
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-05 20:44:48 -0300

    criando objeto pessoa

--- build/aula08.js
+++ build/aula08.js
@@ -1,1 +1,9 @@
 "use strict";
+// type objects
+let pessoa = {
+    nome: 'Luiz',
+    idade: 30,
+    status: 'Ativo'
+};
+console.log(pessoa);
+console.log(typeof pessoa);
---
+++ src/aula08.ts
@@ -0,0 +1,9 @@
+// type objects
+let pessoa:object = {
+    nome: 'Luiz',
+    idade: 30,
+    status: 'Ativo'
+}
+
+console.log(pessoa)
+console.log(typeof pessoa)
