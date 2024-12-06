d817b4f92ae73a82ea14a84a933c9afed64c332b
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-06 15:40:50 -0300

    exemplo1

--- build/aula11.js
+++ build/aula11.js
@@ -1,2 +1,11 @@
 "use strict";
 //type assertion e type casting
+let nvalor;
+let svalor;
+let uvalor;
+uvalor = 10;
+nvalor = 10;
+console.log(nvalor);
+console.log(typeof nvalor);
+console.log(uvalor);
+console.log(typeof uvalor);
--- src/aula11.ts
+++ src/aula11.ts
@@ -1,1 +1,14 @@
 //type assertion e type casting
+let nvalor:number
+let svalor:string
+let uvalor:unknown
+
+uvalor = 10
+
+nvalor = 10
+
+console.log(nvalor)
+console.log(typeof nvalor)
+
+console.log(uvalor)
+console.log(typeof uvalor)
