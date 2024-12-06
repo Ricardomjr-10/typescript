07f5993983010508daa67c6e4febce0111da7db0
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-05 21:00:58 -0300

    criando enumerador

--- build/aula09.js
+++ build/aula09.js
@@ -1,1 +1,12 @@
 "use strict";
+//ENUM- enumaradores
+var diasSemana;
+(function (diasSemana) {
+    diasSemana[diasSemana["domingo"] = 1] = "domingo";
+    diasSemana[diasSemana["segunda"] = 2] = "segunda";
+    diasSemana[diasSemana["terca"] = 3] = "terca";
+    diasSemana[diasSemana["quarta"] = 4] = "quarta";
+    diasSemana[diasSemana["quinta"] = 5] = "quinta";
+    diasSemana[diasSemana["sexta"] = 6] = "sexta";
+    diasSemana[diasSemana["sabado"] = 7] = "sabado";
+})(diasSemana || (diasSemana = {}));
---
+++ src/aula09.ts
@@ -0,0 +1,10 @@
+//ENUM- enumaradores
+enum diasSemana {
+    domingo = 1,
+    segunda = 2,
+    terca = 3,
+    quarta = 4,
+    quinta = 5,
+    sexta = 6,
+    sabado = 7
+}
