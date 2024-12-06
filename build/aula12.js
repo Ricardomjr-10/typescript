02f0421875057274dde2e9e7a3d4e64ed9aeb51e
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-06 15:58:21 -0300

    exemplo1

--- build/aula12.js
+++ build/aula12.js
@@ -1,2 +1,11 @@
 "use strict";
 //funcoes em typescript
+function teste() {
+    console.log('Teste');
+}
+function logar(user, pass) {
+    console.log(`User:${user}
+                Senha:${pass}`);
+}
+teste();
+logar('bruno', '123');
--- src/aula12.ts
+++ src/aula12.ts
@@ -1,1 +1,13 @@
 //funcoes em typescript
+function teste():void{ // void - retorna nada
+ console.log('Teste')
+}
+
+function logar(user:string, pass:string):void {
+    console.log(`User:${user}
+                Senha:${pass}`)
+}
+
+teste()
+
+logar('bruno', '123')
