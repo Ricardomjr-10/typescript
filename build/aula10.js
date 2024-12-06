3163ad2f6e5622bcff83c442ec06f567dc566571
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-06 15:26:10 -0300

    exemplo1

--- build/aula10.js
+++ build/aula10.js
@@ -1,2 +1,7 @@
 "use strict";
 // null - undefined - unknown
+//null - valor vazio - nulo
+// undefined - indefinido -sem valor associado
+let vnome;
+vnome = null;
+console.log(vnome);
--- src/aula10.ts
+++ src/aula10.ts
@@ -1,1 +1,8 @@
 // null - undefined - unknown
+//null - valor vazio - nulo
+// undefined - indefinido -sem valor associado
+
+
+let vnome: string | null 
+vnome = null
+console.log(vnome)
