77aad46c4eebef3d4b16abb4bdaa34c6aeb07ed5
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-05 21:08:41 -0300

    criando enum de cores

--- build/aula09.js
+++ build/aula09.js
@@ -10,3 +10,17 @@ var diasSemana;
     diasSemana[diasSemana["sexta"] = 6] = "sexta";
     diasSemana[diasSemana["sabado"] = 7] = "sabado";
 })(diasSemana || (diasSemana = {}));
+// console.log(diasSemana.domingo)
+// console.log(diasSemana['domingo'])
+// console.log(diasSemana[1])
+const data = new Date();
+// console.log(data.getDate())
+// console.log(diasSemana[data.getDay() + 1])
+var cores;
+(function (cores) {
+    cores["branco"] = "#fff";
+    cores["preto"] = "#000";
+    cores["azul"] = "#00f";
+    cores["verde"] = "#0f0";
+    cores["vermelho"] = "#f00";
+})(cores || (cores = {}));
--- src/aula09.ts
+++ src/aula09.ts
@@ -8,3 +8,19 @@ enum diasSemana {
     sexta = 6,
     sabado = 7
 }
+
+// console.log(diasSemana.domingo)
+// console.log(diasSemana['domingo'])
+// console.log(diasSemana[1])
+
+const data = new Date()
+// console.log(data.getDate())
+// console.log(diasSemana[data.getDay() + 1])
+
+enum cores {
+    branco = '#fff',
+    preto = '#000',
+    azul = '#00f',
+    verde = '#0f0',
+    vermelho = '#f00'
+}
