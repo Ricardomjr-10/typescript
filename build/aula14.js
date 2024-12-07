09d3a17595f40054a0f694e5961aadbe4eeb4bec
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-07 13:35:30 -0300

    function soma1

--- build/aula14.js
+++ build/aula14.js
@@ -6,3 +6,7 @@ test('testando');
 test('testando');
 test('testando');
 test();
+const soma1 = (n1, n2) => {
+    return n1 + n2;
+};
+console.log(soma1(25, 34));
--- src/aula14.ts
+++ src/aula14.ts
@@ -6,3 +6,9 @@ test('testando')
 test('testando')
 test('testando')
 test()
+
+const soma1 = (n1:number, n2:number):number => {
+    return n1 + n2
+}
+
+console.log(soma1(25, 34))
