a8e60faf5bb01233af883747a6209f926190df6c
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-07 13:50:46 -0300

    rest e spread function

--- build/aula15.js
+++ build/aula15.js
@@ -1,2 +1,6 @@
 "use strict";
 // Rest e espread operator
+function soma2(v1, v2) {
+    return v1 + v2;
+}
+console.log(soma2(10, 20));
--- src/aula15.ts
+++ src/aula15.ts
@@ -1,1 +1,6 @@
 // Rest e espread operator
+function soma2 (v1:number, v2:number) {
+    return v1 + v2
+}
+
+console.log(soma2(10, 20))
