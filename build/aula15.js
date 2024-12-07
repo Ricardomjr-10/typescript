f29e4950e940dcf84a7980f8a0e1ba13397b76d5
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-07 13:55:02 -0300

    func com rest

--- build/aula15.js
+++ build/aula15.js
@@ -1,6 +1,13 @@
 "use strict";
 // Rest e espread operator
-function soma2(v1, v2) {
-    return v1 + v2;
+function soma2(v1, v2, v3) {
+    return v1 + v2 + v3;
 }
-console.log(soma2(10, 20));
+console.log(soma2(10, 20, 30));
+// rest
+function soma3(...n) {
+    let s = 0;
+    n.forEach(e => s += e);
+    return s;
+}
+console.log(soma3(10, 20));
--- src/aula15.ts
+++ src/aula15.ts
@@ -1,6 +1,15 @@
 // Rest e espread operator
-function soma2 (v1:number, v2:number) {
-    return v1 + v2
+function soma2 (v1:number, v2:number, v3:number) {
+    return v1 + v2 + v3
 }
 
-console.log(soma2(10, 20))
+console.log(soma2(10, 20, 30))
+
+// rest
+function soma3 (...n:number[]) {
+    let s:number = 0
+    n.forEach(e => s += e)
+    return s
+}
+
+console.log(soma3(10,20))
