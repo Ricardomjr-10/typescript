9f5f1c2e95f9a9452b064f7afa75d79dcbe8d18d
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-08 15:13:48 -0300

    Herancas

--- build/aula20.js
+++ build/aula20.js
@@ -8,4 +8,11 @@ class Conta {
         this.titular = titular;
     }
 }
+class ContaPF extends Conta {
+}
+class ContaPJ extends Conta {
+}
 const conta1 = new Conta(123456789, 'Breno');
+const conta2 = new ContaPF(1234567, 'Rafael');
+const conta3 = new ContaPJ(123456, 'Zuleide');
+console.log(conta1, conta2);
--- src/aula20.ts
+++ src/aula20.ts
@@ -1,8 +1,8 @@
 //herança - classe que herda as mesmas caracteristicas da casse pai
 
 class Conta {
-    numero: number
-    titular: string
+    public numero: number
+    public titular: string
 
     constructor(numero: number, titular: string){
         this.numero = numero
@@ -10,4 +10,16 @@ class Conta {
     }
 }
 
+class ContaPF extends Conta {//heredan caracteristicas da conta pai
+
+}
+
+class ContaPJ extends Conta {//heredan caracteristicas da conta pai
+
+}
+
 const conta1 = new Conta(123456789, 'Breno')
+const conta2  = new ContaPF(1234567, 'Rafael')
+const conta3 = new ContaPJ(123456, 'Zuleide')
+
+console.log(conta1, conta2)
