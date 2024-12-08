c680a3204e048121fbc2c0467681b91fb7bdc3a6
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-08 14:31:49 -0300

    add metodos ligar e desligar

--- build/aula16-17-18.js
+++ build/aula16-17-18.js
@@ -16,14 +16,23 @@ class Computador {
         console.log(`Nome: ${this.nome}`);
         console.log(`Ram: ${this.ram}`);
         console.log(`CPU: ${this.cpu}`);
-        console.log(`Ligado: ${this.ligado}`);
+        console.log(`Ligado: ${this.ligado ? 'Sim' : 'Não'}`); //ternario
         console.log('------------------------');
+    }
+    ligar() {
+        this.ligado = true;
+    }
+    desligar() {
+        this.ligado = false;
     }
 }
 //instaciar uma classe
 const comp1 = new Computador("comp1", 64, 4);
 const comp2 = new Computador("comp2", 32, 5);
 const comp3 = new Computador("comp3", 128, 10);
+comp1.ligar();
+comp3.ligar();
+comp1.desligar();
 comp1.info();
 comp2.info();
 comp3.info();
