cafc1eb41078ef57fdb4d416d27e15c3c2937344
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-07 23:03:28 -0300

    terminado

--- build/aula16-17.js
+++ build/aula16-17.js
@@ -1,19 +1,21 @@
 "use strict";
 //P.O.O - classes
 class Computador {
-    nome = 'computador 1';
-    ram = 0;
-    cpu = 0;
-    ligado = true;
-    constructor(nome) {
+    nome;
+    ram;
+    cpu;
+    ligado;
+    constructor(nome, ram, cpu) {
         this.nome = nome;
-        console.log('objeto criado');
+        this.ram = ram;
+        this.cpu = cpu;
+        this.ligado = false;
     }
 }
 //instaciar uma classe
-const comp1 = new Computador("comp1");
-const comp2 = new Computador("comp2");
-const comp3 = new Computador("comp3");
-console.log(comp1.nome);
-console.log(comp2.nome);
-console.log(comp3.nome);
+const comp1 = new Computador("comp1", 64, 4);
+const comp2 = new Computador("comp2", 32, 5);
+const comp3 = new Computador("comp3", 128, 10);
+console.log(comp1);
+console.log(comp2);
+console.log(comp3);
--- src/aula16-17.ts
+++ src/aula16-17.ts
@@ -1,23 +1,25 @@
 //P.O.O - classes
-class Computador {
-    nome: string = 'computador 1'
-    ram: number=0
-    cpu: number=0
-    ligado: boolean= true
-    
-    constructor(nome:string) {
-        this.nome = nome
-        console.log('objeto criado')
-    }
 
+class Computador {
+        nome: string;
+        ram :number;
+        cpu :number
+        ligado: boolean
+
+    constructor( nome:string, ram:number, cpu:number) {
+        this.nome = nome;
+        this.ram = ram;
+        this.cpu = cpu
+        this.ligado = false
+    }
 }
 
 //instaciar uma classe
-const comp1 = new Computador("comp1")
-const comp2 = new Computador("comp2")
-const comp3 = new Computador("comp3")
+const comp1 = new Computador("comp1", 64, 4)
+const comp2 = new Computador("comp2", 32, 5)
+const comp3 = new Computador("comp3", 128, 10)
 
 
-console.log(comp1.nome)
-console.log(comp2.nome)
-console.log(comp3.nome)
+console.log(comp1)
+console.log(comp2)
+console.log(comp3)
