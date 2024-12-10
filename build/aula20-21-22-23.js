4abf7f0b6dd15d7902f4b0f2011d5d0c80af1343
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-09 21:56:29 -0300

    heranca parte 4

--- build/aula20-21-22-23.js
+++ build/aula20-21-22-23.js
@@ -24,6 +24,11 @@ class ContaPF extends Conta {
         this.cpf = cpf;
         console.log(`Conta PF criada:${titular}`); // atraves do proteced permiti o acesso
     }
+    info() {
+        super.info();
+        console.log(`CPF: ${this.cpf}`);
+        console.log('------------------------');
+    }
 }
 class ContaPJ extends Conta {
     cnpj;
@@ -32,11 +37,16 @@ class ContaPJ extends Conta {
         this.cnpj = cnpj;
         console.log(`Conta PJ criada:${titular}`); // ateaves do proteced permiti o acesso
     }
+    info() {
+        super.info();
+        console.log(`CNPJ: ${this.cnpj}`);
+        console.log('------------------------');
+    }
 }
 const conta1 = new Conta('Breno');
 const conta2 = new ContaPF(1231212, 'Rafael');
 const conta3 = new ContaPJ(123124, 'Zuleide');
 // console.log(conta1, conta2)
-conta1.info();
+//conta1.info()
 conta2.info();
 conta3.info();
--- src/aula20-21-22-23.ts
+++ src/aula20-21-22-23.ts
@@ -12,6 +12,12 @@ class Conta {
     private gerarNumeroConta():number {
         return Math.floor(Math.random() * 10000) + 1
     }
+
+    protected info() {
+        console.log(`titular: ${this.titular}`)
+        console.log(`numero: ${this.numero}`)
+        console.log('------------------------')
+    }
 }
 
 class ContaPF extends Conta {//heredan caracteristicas da conta pai
@@ -20,6 +26,12 @@ class ContaPF extends Conta {//heredan caracteristicas da conta pai
         super(titular)
         this.cpf = cpf
         console.log(`Conta PF criada:${titular}`) // atraves do proteced permiti o acesso
+    }
+
+    info() {
+        super.info()
+        console.log(`CPF: ${this.cpf}`)
+        console.log('------------------------')
     }
 }
 
@@ -30,11 +42,21 @@ class ContaPJ extends Conta {//heredan caracteristicas da conta pai
         this.cnpj = cnpj
         console.log(`Conta PJ criada:${titular}`)// ateaves do proteced permiti o acesso
     }
+
+    info() {
+        super.info()
+        console.log(`CNPJ: ${this.cnpj}`)
+        console.log('------------------------')
+    }
 }
 
 const conta1 = new Conta('Breno')
 const conta2  = new ContaPF(1231212, 'Rafael')
 const conta3 = new ContaPJ(123124, 'Zuleide')
 
-console.log(conta1, conta2)
+// console.log(conta1, conta2)
+
+//conta1.info()
+conta2.info()
+conta3.info()
 
