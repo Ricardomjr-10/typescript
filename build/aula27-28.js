2034a63f4682b28a7714520034eaec4f214eb172
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-10 21:35:34 -0300

    interface parte 2

---
+++ build/aula27-28.js
@@ -0,0 +1,30 @@
+"use strict";
+let curso1; // permitir que a variavel curso1 seja do tipo curso1
+let curso2;
+let curso3;
+// let curso2: {
+//     titulo: string,
+//     des: string,
+//     aula: number,
+//     maxAlunos: number,
+// }
+curso1 = {
+    titulo: 'typeScript',
+    des: 'curso de typescript',
+    aula: 12,
+    maxAlunos: 30
+};
+curso2 = {
+    titulo: 'javascript',
+    des: 'curso de javascript',
+    aula: 120,
+    maxAlunos: 60
+};
+curso3 = {
+    titulo: 'python',
+    des: 'curso de python',
+    aula: 100,
+};
+console.log(curso1);
+console.log(curso2);
+console.log(curso3);
