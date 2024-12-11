dc4143dbfc04a74aedb7c195ab61133175a2838d
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-10 21:24:29 -0300

    criando interface

--- build/aula27.js
+++ build/aula27.js
@@ -1,2 +1,20 @@
 "use strict";
-// interface - objetos literais
+// let curso2: {
+//     titulo: string,
+//     des: string,
+//     aula: number,
+//     maxAlunos: number,
+// }
+curso1 = {
+    titulo: 'typeScript',
+    des: 'curso de typescript',
+    aula: 12,
+    maxAlunos: 30
+};
+// curso2 = {
+//     titulo: 'javascript',
+//     des: 'curso de javascript',
+//     aula: 120,
+//     maxAlunos: 60
+// }
+console.log(curso1);
--- src/aula27.ts
+++ src/aula27.ts
@@ -1,1 +1,30 @@
 // interface - objetos literais
+interface curso1 {
+    titulo: string,
+    des: string,
+    aula: number,
+    maxAlunos: number,
+}
+
+// let curso2: {
+//     titulo: string,
+//     des: string,
+//     aula: number,
+//     maxAlunos: number,
+// }
+
+curso1 = {
+    titulo: 'typeScript',
+    des: 'curso de typescript',
+    aula: 12,
+    maxAlunos: 30
+}
+
+// curso2 = {
+//     titulo: 'javascript',
+//     des: 'curso de javascript',
+//     aula: 120,
+//     maxAlunos: 60
+// }
+
+console.log(curso1)
