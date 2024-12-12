ed98633931e8caa59599d1a538f5c42dc78f2e42
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-11 21:10:56 -0300

    generics em classes

--- build/aula29.js
+++ build/aula29.js
@@ -1,8 +1,17 @@
 "use strict";
 // generics - tipo genérico - estrutura de dados genérico
-function f_teste(v) {
-    return v;
+function f_teste(v, r) {
+    return r;
 }
-console.log(f_teste(10));
-console.log(f_teste('10'));
-console.log(f_teste(true));
+console.log(f_teste(10, '8')); // indica o tipo de dado
+console.log(f_teste('10', 12));
+console.log(f_teste(true, false));
+class C_teste {
+    valor;
+    constructor(valor) {
+        this.valor = valor;
+    }
+}
+const ct1 = new C_teste(10);
+const ct2 = new C_teste('luiz');
+console.log(ct1.valor);
--- src/aula29.ts
+++ src/aula29.ts
@@ -1,7 +1,19 @@
 // generics - tipo genérico - estrutura de dados genérico
-function f_teste<T>(v:T):T { // adiciona a letra T para indicar o tipo de dado
-    return v
+function f_teste<T,U>(v:T,r:U):U { // adiciona a letra T para indicar o tipo de dado
+    return r
 }
-console.log(f_teste<number>(10))
-console.log(f_teste<string>('10'))
-console.log(f_teste<boolean>(true))
+console.log(f_teste<number,string>(10,'8')) // indica o tipo de dado
+console.log(f_teste<string,number>('10',12))
+console.log(f_teste<boolean,boolean>(true,false))
+
+class C_teste<T> {
+    public valor:T
+    constructor(valor:T){
+        this.valor = valor
+    }
+}
+
+const ct1 = new C_teste<number>(10)
+const ct2 = new C_teste<string>('luiz')
+
+console.log(ct1.valor)
