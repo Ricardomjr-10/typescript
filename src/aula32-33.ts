22b09ff331233667d6f25091a435ac09f421faf7
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-11 22:11:04 -0300

    namespce parte 2 exemplos

--- src/aula32-33.ts
+++ src/aula32-33.ts
@@ -1,11 +1,11 @@
 //namespaces - agrupamento de classes e metodos dentro de um espaco especifico
 namespace Veiculos {
-    export class Carro {// é preciso usar o export para exportar a classe, por causa do namespace
-        nome:string
+    export abstract class Carro {// é preciso usar o export para exportar a classe, por causa do namespace
+        nome: string
         motor: Motores.Motor
-        constructor(nome:string) {
+        constructor(nome: string) {
             this.nome = nome
-            this.motor = new Motores.Motor(1000)
+            this.motor = new Motores.Motor(3,100)
         }
     }
 }
@@ -13,15 +13,31 @@ namespace Veiculos {
 namespace Motores {
 
     class Turbo {
-        pot:number
-        constructor(pot:number) {
+        private pot: number
+        constructor(pot: number) {
             this.pot = pot
         }
+        get potencia() {
+            return this.pot
+        }
     }
-   export class Motor {
-        pot:number
-        constructor(pot:number) {
-            this.pot = pot
+    export class Motor {
+        private ligado: boolean
+        private cilindros: number
+        private pot: number
+        constructor(cilindros: number, pot: number, turbo?:Turbo) {
+            this.ligado = false
+            this.cilindros = cilindros
+            this.pot = pot + (turbo? turbo.potencia : 0)
+        }
+        set liga(ligado:boolean) {
+            this.ligado = ligado
+        }
+        get liga(){
+           return this.ligado
+        }
+        get potencia() {
+            return this.pot
         }
     }
 }
