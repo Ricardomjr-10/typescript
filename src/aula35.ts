b9ebadc4aa46149e1d4076772f3b1e9b0488aaeb
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-12 22:29:43 -0300

    desestruturacao de array

--- src/aula35.ts
+++ src/aula35.ts
@@ -1,3 +1,13 @@
 //desestruturacao - oque é?
 //um jeito facil de extrair valores de um array ou objeto
 
+//let aa,bb,cc,dd
+let av = [10,20,30,40]
+
+// aa = av[0]
+// bb = av[1]
+// cc = av[2]
+// dd = av[3]
+
+//desestruturacao de array
+let [aa,bb,cc,dd] = av
