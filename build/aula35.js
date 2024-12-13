af098707a475f21cde7b0614ebf3f96e030a9e04
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-12 22:35:32 -0300

    desestruturacao de objeto

--- build/aula35.js
+++ build/aula35.js
@@ -12,3 +12,12 @@ let [aa, bb, cc, dd] = av;
 let [aaa, bbb, ccc, ddd] = ["luiz", "maria", "joao", "pedro"];
 console.log(aa, bb, cc, dd);
 console.log(aaa, bbb, ccc, ddd);
+//desetruturacao de objeto
+const obj = {
+    cor1: "azul",
+    cor2: "vermelho",
+    cor3: "amarelo",
+    cor4: "verde"
+};
+let { cor1, cor2, cor3, cor4 } = obj;
+console.log(cor1, cor2, cor3, cor4);
--- src/aula35.ts
+++ src/aula35.ts
@@ -15,3 +15,15 @@ let [aaa,bbb,ccc,ddd] = ["luiz", "maria", "joao", "pedro"]
 
 console.log(aa,bb,cc,dd)
 console.log(aaa,bbb,ccc,ddd)
+
+//desetruturacao de objeto
+const obj = {
+    cor1: "azul",
+    cor2: "vermelho",
+    cor3: "amarelo",
+    cor4: "verde"
+}
+
+let {cor1,cor2,cor3,cor4} = obj
+
+console.log(cor1,cor2,cor3,cor4)
