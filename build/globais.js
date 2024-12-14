0e91875b8a57963c1848dee944273eeed71ccbcf
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-13 23:13:00 -0300

    curso finalizado

--- build/globais.js
+++ build/globais.js
@@ -6,5 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Globais = void 0;
 class Globais {
     static teste = 0;
+    static ateste = [];
+    static objeto = {
+        nome: 'Ricardo',
+        canal: 'CFB Cursos'
+    };
 }
 exports.Globais = Globais;
--- src/globais.ts
+++ src/globais.ts
@@ -4,5 +4,9 @@
 
 export abstract class Globais {
     public static teste: number = 0
-
+    public static ateste: number[] = []
+    public static objeto  = {
+        nome: 'Ricardo',
+        canal: 'CFB Cursos'
+    }
 }
