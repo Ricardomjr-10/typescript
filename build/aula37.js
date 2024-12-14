8f5c9cc3b7cdb89d95a6f230ca8decc036b8b1b7
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-13 23:03:56 -0300

    exemplo1

--- build/aula37.js
+++ build/aula37.js
@@ -2,3 +2,7 @@
 //static poo - propriedades ou  metodos estaticos - menbros associados a classe e nao a instancia
 //mesmo endereço de memoria - apontando para o mesmo endereço de memoria
 //globais
+Object.defineProperty(exports, "__esModule", { value: true });
+const globais_1 = require("./globais");
+console.log(globais_1.Globais.teste);
+globais_1.Globais.teste = 10;
--- src/aula37.ts
+++ src/aula37.ts
@@ -1,3 +1,9 @@
 //static poo - propriedades ou  metodos estaticos - menbros associados a classe e nao a instancia
 //mesmo endereço de memoria - apontando para o mesmo endereço de memoria
 //globais
+
+import { Globais} from './globais'
+
+console.log(Globais.teste)
+
+Globais.teste = 10
