b05a444852ca79e0ba63600820d328a76354bb37
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-16 15:50:16 -0300

    exercicio2 concluido

--- exercicios/exercicio2.ts
+++ exercicios/exercicio2.ts
@@ -9,3 +9,34 @@
 // sacar(valor: number): Decrementa o saldo se o valor for válido.
 // depositar(valor: number): Incrementa o saldo.
 // exibirSaldo(): Imprime o saldo no console.
+
+class ContaBancaria {
+    numeroConta: number
+    saldo: number
+    constructor(numeroConta: number, saldo: number) {
+        this.numeroConta = numeroConta
+        this.saldo = saldo
+    }
+    sacar(valor: number) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor
+        } else {
+            console.log("Saldo insuficiente")
+        }
+    }
+    depositar(valor: number) {
+        this.saldo += valor
+    }
+    exibirSaldo() {
+        console.log(this.saldo)
+    }
+}
+
+const novaConta = new ContaBancaria(123, 1000)
+
+novaConta.sacar(500)
+novaConta.sacar(500)
+
+novaConta.depositar(2000)
+
+novaConta.exibirSaldo()
