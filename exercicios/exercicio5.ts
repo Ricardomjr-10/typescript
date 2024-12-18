83f6609cc44dadd6a0098e55e4f5f5b4259cd180
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-18 10:04:59 -0300

    add interface

---
+++ exercicios/exercicio5.ts
@@ -0,0 +1,57 @@
+// Exercício 2: Figura Geométrica
+// Enunciado:
+// Crie uma interface FiguraGeometrica com os métodos calcularArea()
+//  e calcularPerimetro(). Crie classes concretas Circulo, 
+// Quadrado e Retangulo que implementem essa interface.
+
+interface FiguraGeometrica {
+    calcularArea(): number
+    calcularPerimetro(): number
+}
+
+class Circulo implements FiguraGeometrica {
+    raio: number
+    constructor(raio: number) {
+        this.raio = raio
+    }
+    calcularArea(): number {
+        return Math.PI * this.raio ** 2
+    }
+    calcularPerimetro(): number {
+        return 2 * Math.PI * this.raio
+    }
+}
+
+class Quadrado implements FiguraGeometrica {
+    lado: number
+    constructor(lado:number) {
+        this.lado = lado
+    }
+    calcularArea(): number {
+        return this.lado ** 2
+    }
+    calcularPerimetro(): number {
+        return 4 * this.lado
+    }
+}
+
+class Retangulo implements FiguraGeometrica {
+    largura: number
+    altura: number
+    constructor(largura: number, altura: number) {
+        this.largura = largura
+        this.altura = altura
+    }
+    calcularArea(): number {
+        return this.largura * this.altura
+    }
+    calcularPerimetro(): number {
+        return 2 * (this.largura + this.altura)
+    }
+}
+
+const circulo = new Circulo(5)
+const quadrado = new Quadrado(5)
+const retangulo = new Retangulo(5, 10)
+
+console.log(circulo.calcularArea())
---
+++ exercicios/exercicio6.ts
@@ -0,0 +1,7 @@
+// Exercício 3: Loja de Eletrônicos
+// Enunciado:
+// Crie um sistema para uma loja de eletrônicos utilizando design patterns. 
+// Crie uma classe abstrata ProdutoEletronico com atributos como nome, preco e garantia.
+//  Crie classes concretas como Smartphone, Notebook e Televisão. 
+// Implemente um padrão de fábrica para criar os produtos e 
+// um padrão de observador para notificar clientes sobre promoções.
